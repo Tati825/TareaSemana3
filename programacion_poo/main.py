58a88c525a6d94ab2400e9a1ee4d55a906c949cb
Tati825 <et.espanav@uea.edu.ec>
2026-06-13 22:35:43 -0500

    Create main.py

---
+++ programacion_poo/main.py
@@ -0,0 +1,13 @@
+from mascota import Mascota
+
+# Se crean dos objetos a partir de la clase Mascota
+mascota1 = Mascota("Ares", "Perro", 2)
+mascota2 = Mascota("Silvestre", "Gato", 4)
+
+# Se muestran los dos métodos que son la información y el sonido de la mascota 1
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+# Se muestran los dos métodos que son la información y el sonido de la mascota 2
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
